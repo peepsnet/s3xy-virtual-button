77c2a23b6e39456364cf72fef3e8552c78008974
Don <peepsnet@yahoo.com>
2026-03-07 21:15:40 -0500

    Seeed XIAO ESP32-S3 with TTP223B Capacitive Touch Switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,124 @@
 #include <Arduino.h>
 #include "S3XYButton.h"
+#include <OneButton.h>
 
-const int LED_PIN = 2;
+#define    INPIN                  D8    // INPUT from Switch
+
+long delayLowMin;
+long delayLowMax;
+
+bool powerSwitch                = false;
+
+unsigned long timeNow           = 0;
+unsigned long delayBy           = 0;
+
+OneButton btn;
+
+static void randomize() {
+  delayLowMin                      = random(9500, 10000);
+  delayLowMax                      = random(12000, 12500);
+  delayBy                          = random(delayLowMin, delayLowMax);
+}
+
+
+static void setLED() {
+  if (powerSwitch) {
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  Serial.println("Power LED status set/reset!!");
+}
+
+static void blinkLED(int blink) {
+  for (int i = 1; i <= blink; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);          //LED OFF
+    delay(75);
+    digitalWrite(LED_BUILTIN, LOW);           //LED ON
+    delay(75);
+    if (!powerSwitch) {
+      digitalWrite(LED_BUILTIN,HIGH);           //LED OFF
+      delay(75);
+    }
+  }
+  setLED();
+}
+
+// Handler function for a single click:
+static void pressS3XYBtnNow() {
+  s3xy_send_single();
+  blinkLED(1);
+  Serial.println("S3XY Button Pressed!!");
+}
+
+static void doubleClickS3XYButton() {
+  s3xy_send_double();
+  blinkLED(2);
+  Serial.println("S3XY Button Double Clicked!!");
+}
+
+static void multiClickS3XYButton() {
+  s3xy_send_long();
+  blinkLED(3);
+  Serial.println("S3XY Button Long Clicked!!");
+}
+
+static void toggleOnOff() {
+  powerSwitch = !powerSwitch;
+  blinkLED(2);
+  timeNow = millis();
+  Serial.println("LONG Clicked! Power Toggled!!");
+}
 
 void onConnected() {
-  Serial.println("[user] connected");
-  digitalWrite(LED_PIN, HIGH);
+  blinkLED(2);
+  Serial.println("[user] connected"); 
 }
 
 void onDisconnected() {
+  blinkLED(2);
   Serial.println("[user] disconnected");
-  digitalWrite(LED_PIN, LOW);
 }
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  Serial.begin(115200);
+  delay(1000);
 
   s3xy_on_connect(onConnected);
   s3xy_on_disconnect(onDisconnected);
-
   s3xy_begin("ENH_BTN");
+
+  Serial.println("Setup Begins");
+
+  pinMode(INPIN, INPUT);    
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  timeNow = millis();
+  randomize();
+
+  btn.setup(INPIN, INPUT_PULLUP, false);
+  btn.attachClick(pressS3XYBtnNow);
+  btn.attachDoubleClick(doubleClickS3XYButton);
+  btn.attachLongPressStart(multiClickS3XYButton);
+  btn.attachMultiClick(toggleOnOff);
+  btn.reset();
+
+  blinkLED(2);
+
+  Serial.println("Setup Complete");
+
 }
 
 void loop() {
-  s3xy_loop();
+  //s3xy_loop();
 
-  if (s3xy_ready() && Serial.available()) {
-    char input = Serial.read();
-
-    if (input == 'p') {
-      s3xy_send_single();
-    } else if (input == 'd') {
-      s3xy_send_double();
-    } else if (input == 'l') {
-      s3xy_send_long();
+  if (s3xy_ready() ) {
+    btn.tick();   //Capture control button status
+    
+    if (millis() - timeNow > delayBy && powerSwitch) {
+      pressS3XYBtnNow();
+      timeNow = millis();
+      randomize();
     }
   }
-
-  delay(100);
 }

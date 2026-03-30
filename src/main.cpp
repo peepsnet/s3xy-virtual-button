9f3853e487daa75af161a0dd37e7a542b0f9db3c
Don <peepsnet@yahoo.com>
2026-03-30 15:06:38 -0400

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,64 +9,44 @@
 #include <ESPAsyncWebServer.h>
 #include "S3XYButton.h"
 #include "html.h"
+#include "images.h"
 
 Logger logger(Debug, "[{time}] {level}: [{file}.{function}.{lineno}] {message}");
 
 AsyncWebServer ASYNC_SERVER(80);
 
-#define    INPIN                  D8    // INPUT from Switch
+//#define    INPIN                  D8    // INPUT from Switch
 
 #define SCREEN_WIDTH              128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT             64 // OLED display height, in pixels
 // declare an SSD1306 display object connected to I2C
-Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+#define LOGO_W_S3XY   128
+#define LOGO_H_S3XY   50
 
-const char* AP_SSID               = "S3XY_Buttton";
-const char* AP_PASSWD             = "9548951411!";
-String WiFI_SSID                  = "SheltAirGuestWiFi";
-String WiFi_PASSWD                = "1234567890!";
-//String WiFI_SSID                = "3844-Main-2.4G";
-//String WiFi_PASSWD              = "";
-const char* hostName              = "s3xy";
+const char* AP_SSID                 = "S3XY_Buttton";
+const char* AP_PASSWD               = "9548951411!";
+String WiFI_SSID                    = "SheltAirGuestWiFi";
+String WiFi_PASSWD                  = "1234567890!";
+//String WiFI_SSID                  = "3844-Main-2.4G";
+//String WiFi_PASSWD                = "";
+const char* hostName                = "s3xy";
 
 long delayLowMin;
 long delayLowMax;
 
-bool powerSwitch                  = false;
-bool useSwitch                    = true;
+bool powerSwitch                    = false;
+bool useSwitch                      = true;
 
-unsigned long timeNow             = 0;
-unsigned long delayBy             = 0;
+unsigned long timeNow               = 0;
+unsigned long delayBy               = 0;
 
 OneButton btn;
 
 static void randomize() {
-  delayLowMin                      = random(9500, 10000);
-  delayLowMax                      = random(12000, 12500);
-  delayBy                          = random(delayLowMin, delayLowMax);
-}
-
-void oled_init() {
-  int attempts = 0;
-  const int maxAttempts = 5;
-  bool initialized = false;
-
-  while (attempts < maxAttempts && !initialized) {
-      initialized = oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-      if (!initialized) {
-          LOG(Debug, "SSD1306 allocation failed, retrying...");
-          attempts++;
-          delay(1000); // Wait before retrying
-      }
-  }
-
-  if (!initialized) {
-      LOG(Debug, "Failed to initialize SSD1306 after several attempts.");
-  } else {
-      LOG(Debug, "SSD1306 initialized successfully.");
-      oled.clearDisplay();
-      oled.display();
-  }
+  delayLowMin                       = random(8500, 9000);
+  delayLowMax                       = random(11000, 11500);
+  delayBy                           = random(delayLowMin, delayLowMax);
 }
 
 static void setLED() {
@@ -135,7 +115,6 @@ void onS3XYDisconnected() {
 }
 
 String htmlProcessor(const String& var) {
-  //LOG(Debug, "%s", var);
   LOG(Debug, "Replacing: %s", var);
   if (powerSwitch) {
     return "checked";
@@ -143,20 +122,110 @@ String htmlProcessor(const String& var) {
   return " ";
 }
 
+void drawS3XYTitle(String myStr) {
+  display.setFont();
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.print(myStr);
+}
+
+void drawS3XYAPIP(String myIP) {
+  int y = 16;
+  display.setFont();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, y);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.write("AP:");
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setCursor(0, (y+12));     
+  display.print(myIP);
+}
+
+void drawS3XYDHCPIP(const String& myIP) {
+  int y = 44;
+  display.setFont();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, y);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.write("DHCP:");
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setCursor(0, (y+12));     
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.print(myIP);
+}
+
+void drawS3XYbitmap(void) {
+  display.clearDisplay();
+  display.drawBitmap(
+    (display.width()  - LOGO_W_S3XY ) / 2,
+    (display.height() - LOGO_H_S3XY) / 2,
+    image_data_Image, LOGO_W_S3XY, LOGO_H_S3XY, 1);
+  display.display();
+  delay(1500);
+}
+
+void d(char myChar) {
+  display.setFont();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, ((SCREEN_HEIGHT/2)+6) );   
+  display.write(myChar);
+}
+
+void oled_init() {
+  int attempts = 0;
+  const int maxAttempts = 5;
+  bool initialized = false;
+
+  while (attempts < maxAttempts && !initialized) {
+      initialized = display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+      if (!initialized) {
+          LOG(Debug, "SSD1306 allocation failed, retrying...");
+          attempts++;
+          delay(1000); // Wait before retrying
+      }
+  }
+
+  if (!initialized) {
+      LOG(Debug, "Failed to initialize SSD1306 after several attempts.");
+  } else {
+      LOG(Debug, "SSD1306 initialized successfully.");
+      display.clearDisplay();
+      display.display();
+  }
+}
+
+String myAPIP = "";
+String myAPMAC = "";
+String myWIFIIP= "";
+String myWIFIMAC = "";
+
 void WiFiEvent(WiFiEvent_t event) {
   switch(event) {
       case ARDUINO_EVENT_WIFI_STA_CONNECTED:
-        LOG(Debug, "Connected to AP: %s", "test:" );
+        LOG(Debug, "Connected to Router");
+        drawS3XYDHCPIP("Obtaining IP...");
+        display.display();
         break;
 
       case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-        LOG(Debug, "DHCP WiFi IP: %s", WiFi.localIP().toString().c_str() );
-        LOG(Debug, "WiFi Mac Address: %s", WiFi.macAddress().c_str() );
+        myWIFIIP= WiFi.localIP().toString().c_str();
+        myWIFIMAC = WiFi.macAddress().c_str();
+        LOG(Debug, "DHCP WiFi IP: %s",  myWIFIIP);
+        LOG(Debug, "WiFi Mac Address: %s",  myWIFIMAC);
+        drawS3XYDHCPIP(myWIFIIP);
+        display.display();
         break;
 
       case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
         LOG(Debug, "Not Connected to a WiFI Router/AP. Trying to Reconnecting...");
         WiFi.begin(WiFI_SSID, WiFi_PASSWD);
+        drawS3XYDHCPIP("Connecting...");
+        display.display();
         break;
 
       case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
@@ -164,12 +233,16 @@ void WiFiEvent(WiFiEvent_t event) {
         break;
       
       case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED:
-        LOG(Debug, "Someone disconnected to the AP...");
+        LOG(Debug, "Someone disconnected from the AP...");
         break;
 
       case ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED:
-        LOG(Debug, "AP IP Assigned: %s", WiFi.softAPIP().toString());
-
+        myAPIP = WiFi.softAPIP().toString();
+        myAPMAC = WiFi.softAPmacAddress().c_str();
+        LOG(Debug, "AP IP address: %s", myAPIP );
+        LOG(Debug, "AP MAC Address: %s", myAPMAC );
+        drawS3XYAPIP(myAPIP);
+        display.display();
         break;
 
       default:
@@ -179,28 +252,40 @@ void WiFiEvent(WiFiEvent_t event) {
 
 void setup() {  
   Serial.begin(115200);
+  LOG(Debug, "Begining...");
   blinkLED(3);
+  
+  oled_init();
+  delay(500);
 
-  oled_init();  
-
-  delay(1000);
-  oled.clearDisplay();
+  LOG(Debug, "Display Logo!");
+  display.clearDisplay();
+  //drawS3XYbitmap();
+  display.display();
+  
+  display.clearDisplay();
+  drawS3XYTitle("Loading...");
+  display.display();
 
   LOG(Debug, "Power on. Setup Begins");
   LOG(Debug, "Setting up AP");
+  drawS3XYAPIP("Starting...");
+  display.display();
+  delay(1500);
+
   WiFi.setHostname(hostName);
   WiFi.onEvent(WiFiEvent);
   WiFi.mode(WIFI_AP_STA);
-  LOG(Debug, "AP IP address: %s", WiFi.softAPIP().toString() );
-  LOG(Debug, "AP MAC Address: %s", WiFi.softAPmacAddress().c_str() );
-
   delay(1000);
 
   WiFi.begin(WiFI_SSID, WiFi_PASSWD);
   LOG(Debug, "Connectiong to WiFi Router if available...");
+  drawS3XYDHCPIP("Connection...");
+  display.display();
+
   int x = 0;
-  while ( (WiFi.status() != WL_CONNECTED) && (x < 10) ) {
-    delay(1000);
+  while ( (WiFi.status() != WL_CONNECTED) && (x < 40) ) {
+    delay(250);
     LOG(Debug, ".%d", x);
     x++;
   }
